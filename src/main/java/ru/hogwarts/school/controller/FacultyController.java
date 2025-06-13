2a41d31b351f6ca89998cc7c6f5852abe3090edd
byorck <work-byorck@yandex.ru>
2025-06-13 22:57:46 +0300

    HW-1 ver.1

---
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -0,0 +1,63 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("faculty")
+public class FacultyController {
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+
+    @PostMapping
+    /** Create http://localhost:8080/faculty **/
+    public Faculty createFaculty(@RequestBody Faculty faculty) {
+        return facultyService.createFaculty(faculty);
+    }
+
+    @GetMapping("{id}")
+    /** Read http://localhost:8080/faculty/{id} **/
+    public ResponseEntity<Faculty> getFacultyInfo(@PathVariable Long id) {
+        Faculty faculty = facultyService.findFaculty(id);
+        if (faculty == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(faculty);
+    }
+
+    @PutMapping
+    /** Update http://localhost:8080/faculty **/
+    public ResponseEntity<Faculty> editFacultyInfo(@RequestBody Faculty faculty) {
+        Faculty foundFaculty = facultyService.editFaculty(faculty);
+        if (foundFaculty == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(foundFaculty);
+    }
+
+    @DeleteMapping("{id}")
+    /** Delete http://localhost:8080/faculty/{id} **/
+    public Faculty deleteFaculty(@PathVariable Long id) {
+        return facultyService.deleteFaculty(id);
+    }
+
+    @GetMapping
+    /** Read(get all) http://localhost:8080/faculty/ **/
+    public ResponseEntity<Collection<Faculty>> getAllFaculties() {
+        return ResponseEntity.ok(facultyService.getAllFaculties());
+    }
+
+    @GetMapping("/filter/{color}")
+    /** Read(filter by color) http://localhost:8080/faculty/filter/{color} **/
+    public ResponseEntity<Collection<Faculty>> colorFacultiesFilter(@PathVariable String color) {
+        return ResponseEntity.ok(facultyService.colorFacultiesFilter(color));
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -0,0 +1,62 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.StudentService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("student")
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    /** Create http://localhost:8080/student **/
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.createStudent(student);
+    }
+
+    @GetMapping("{id}")
+    /** Read http://localhost:8080/student/{id} **/
+    public ResponseEntity<Student> getStudentInfo(@PathVariable Long id) {
+        Student student = studentService.findStudent(id);
+        if (student == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(student);
+    }
+
+    @PutMapping
+    /** Update http://localhost:8080/student **/
+    public ResponseEntity<Student> editStudentInfo(@RequestBody Student student) {
+        Student foundStudent = studentService.editStudent(student);
+        if (foundStudent == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(foundStudent);
+    }
+
+    @DeleteMapping("{id}")
+    /** Delete http://localhost:8080/student/{id} **/
+    public Student deleteStudent(@PathVariable Long id) {
+        return studentService.deleteStudent(id);
+    }
+
+    @GetMapping
+    /** Read(get all) http://localhost:8080/student/ **/
+    public ResponseEntity<Collection<Student>> getAllStudent() {
+        return ResponseEntity.ok(studentService.getAllStudents());
+    }
+
+    @GetMapping("/filter/{age}")
+    /** Read(filter by age) http://localhost:8080/student/filter/{age} **/
+    public ResponseEntity<Collection<Student>> ageStudentsFilter(@PathVariable int age) {
+        return ResponseEntity.ok(studentService.ageStudentsFilter(age));
+    }
+}
